c679c2803049f7f19865b61cd449f9ef2ec20d70
chhatraliya shraddha <skchhatraliya@gmail.com>
2019-11-12 15:34:18 +0530

    ''

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
 import {
-  createBrowserHistory
-} from "history";
-import {
-  Router,
-  Route,
-  Switch
+    Router, Route, Switch
 } from "react-router-dom";
+
 import './assets/scss/style.scss';
 
 // pages for this product
@@ -16,24 +13,12 @@ import CustomComponents from "./views/custom-components/custom-components.jsx";
 
 var hist = createBrowserHistory();
 
-ReactDOM.render( <
-  Router history = {
-    hist
-  } >
-  <
-  Switch >
-  <
-  Route path = "/custom-components"
-  component = {
-    CustomComponents
-  }
-  /> <
-  Route path = "/"
-  component = {
-    Components
-  }
-  /> < /
-  Switch > <
-  /Router>,
-  document.getElementById("root")
+ReactDOM.render(
+    <Router history={hist}>
+        <Switch>
+            <Route path="/custom-components" component={CustomComponents} />
+            <Route path="/" component={Components} />
+        </Switch>
+    </Router>,
+    document.getElementById("root")
 );
