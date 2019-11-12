ab5c1a3bc3c5f79cd963beaf6fd93d573229b239
niravjoshi87@gmail.com <niravjoshi87@gmail.com>
2019-11-12 11:49:50 +0530

    changes

--- src/components/banner2/banner2.jsx
+++ src/components/banner2/banner2.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { HashLink as Link } from 'react-router-hash-link';
 class HeaderBanner2 extends React.Component {
     render() {
         return (
@@ -9,8 +9,8 @@ class HeaderBanner2 extends React.Component {
                         <div class="col-lg-8 col-md-6 align-self-center text-center" data-aos="fade-up" data-aos-duration="1200">
                             <h1 class="title">Wrapkit Lite</h1>
                             <h4 class="subtitle font-light">Free Reactstrap UI Kit with<br /> Lots of Ready to Use Sections</h4>
-                            <a href="https://wrappixel.com/templates/wrapkit-lite/" className="btn btn-outline-light m-r-20 btn-md m-t-30 font-14">Download Free</a>
-                            <a href="https://wrappixel.com/templates/wrapkit/" className="btn btn-md m-t-30 btn-danger-gradiant font-14">Upgrade To Pro</a>
+                            <a href="https://wrappixel.com/templates/wrapkit-react-lite/" className="btn btn-outline-light m-r-20 btn-md m-t-30 font-14">Download Free</a>
+                            <Link to="/#coming" className="btn btn-md m-t-30 btn-info-gradiant font-14">Upgrade To Pro</Link>
                         </div>
                     </div>
                 </div>
--- src/components/call-to-action/CallToAction.jsx
+++ src/components/call-to-action/CallToAction.jsx
@@ -11,7 +11,7 @@ class CallToAction extends React.Component {
                                 <div>
                                     <h2 className="title text-white font-weight-bold">Pro Version coming soon</h2>
                                     <h6 className="subtitle font-light text-white">We will add Pro Version with tons of great features and multiple category demos which is ready to use...</h6>
-                                    <a href="/#coming" className="btn btn-outline-light m-r-20 btn-md m-t-30 font-14">Upgrade To Pro</a>
+                                    <a href="/#coming" className="btn btn-outline-light m-r-20 btn-md m-t-30 font-14">Comming Soon</a>
                                 </div>
 
                             </div>
